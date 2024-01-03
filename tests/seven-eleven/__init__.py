717f4f19f05b0996ec9f26b14f75fbe54483a5d9
Asada <asadac64@nu.ac.th>
2024-01-03 08:43:29 +0700

    7-11

---
+++ tests/seven-eleven/__init__.py
@@ -0,0 +1,1 @@
+"""Test cases for 7-eleven package."""
---
+++ tests/seven-eleven/test_7-eleven.py
@@ -0,0 +1,13 @@
+"""Test cases for 7-11."""
+
+# Standard libraryy
+
+# 3rd Party library
+import pytest
+
+# Project library
+#from seven-eleven.seven from print_7_eleven
+
+#----------------------------------------------------
+def test_print_7_eleven():
+    """Print 7-eleven"""
